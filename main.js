b8a82db5d20047dc32e7262ab30afe3ccddf9bef
Steven <stevenescobar131@gmail.com>
2022-04-14 09:30:59 -0500

    working on project

--- main.js
+++ main.js
@@ -6,13 +6,13 @@ function computerPlay(arr) {
     return random;
 }
 
-let round = 0; let win = 0; let lose = 0;
+let round = 0; let win = 0; let lose = 0; let tie = 0;
 
 /*creating an instance div_o that gets id "divo"*/
 let div_o = document.getElementById('divo');
 
 /*creating instances that get id of rounds played, wins, loses*/
-let rounds_o = document.getElementById('roundso');
+let round_o = document.getElementById('roundo');
 let win_o = document.getElementById('wino');
 let lose_o = document.getElementById('loseo');
 
@@ -20,32 +20,50 @@ let lose_o = document.getElementById('loseo');
 function playRound(playerSelection, computerSelection) {
     if ( (playerSelection == 'ROCK') && (computerSelection == 'paper') ) {
         div_o.textContent = 'You Lose! Paper beats Rock';
+        round++;
+        lose++;
     }
     else if ( (playerSelection == 'ROCK') && (computerSelection == 'scissor') ) {
         div_o.textContent = 'You Win! Rock beats Scissor';
+        round++;
+        win++;
     }
     else if ( (playerSelection == 'ROCK') && (computerSelection == 'rock') ) {
         div_o.textContent = 'You Tie! Rock = Rock';
+        round++;
+        tie++;
     }
 
     else if ( (playerSelection == 'PAPER') && (computerSelection == 'rock') ) {
         div_o.textContent = 'You Win! Paper beats Rock';
+        round++;
+        win++;
     }
     else if ( (playerSelection == 'PAPER') && (computerSelection == 'scissor') ) {
         div_o.textContent = 'You Lose! Scissor beats Paper';
+        round++;
+        lose++;
     }
     else if ( (playerSelection == 'PAPER') && (computerSelection == 'paper') ) {
         div_o.textContent = 'You Tie! Paper = Paper';
+        round++;
+        tie++;
     }
 
     else if ( (playerSelection == 'SCISSOR') && (computerSelection == 'paper') ) {
         div_o.textContent = 'You Win! Scissor beats Paper';
+        round++;
+        win++;
     }
     else if ( (playerSelection == 'SCISSOR') && (computerSelection == 'rock') ) {
         div_o.textContent = 'You Lose! Rock beats Scissor';
+        round++;
+        lose++;
     }
     else if ( (playerSelection == 'SCISSOR') && (computerSelection == 'scissor') ) {
         div_o.textContent = 'You Tie! Scissor = Scissor';
+        round++;
+        tie++;
     }
 }
 
